950303c53c4430df455f64a5a58b1760d7a898a8
David Boles <david.e.boles22@gmail.com>
2015-11-19 13:04:12 -0800

    Testing

--- src/pageextractor/start/Start.java
+++ src/pageextractor/start/Start.java
@@ -1,10 +1,15 @@
 package pageextractor.start;
 
+import com.deb.lib.image.BGRAYImage;
+import com.deb.lib.image.IARGBImage;
+import com.deb.lib.program.ProgramFs;
+
 public class Start {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		IARGBImage cI = new IARGBImage(ProgramFs.getProgramFile("testing/test.png"));
+		BGRAYImage bI = new BGRAYImage(cI);
+		bI.writeImage("png", ProgramFs.getProgramFile("testing/testgray.png"));
 	}
 
 }
